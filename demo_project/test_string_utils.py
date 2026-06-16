bb6eb8e3f1d2f57df5c10d986cd87539a1310f13
PROVER529 <theprove1453@gmail.com>
2026-06-16 19:41:26 +0800

    Update test_string_utils.py

--- demo_project/test_string_utils.py
+++ demo_project/test_string_utils.py
@@ -2,23 +2,23 @@ import unittest
 
 from string_utils import normalize_title
 
+
 class NormalizeTitleTest(unittest.TestCase):
-def test_trims_outer_whitespace(self) -> None:
-self.assertEqual(normalize_title("  hello world  "), "Hello World")
+    def test_trims_outer_whitespace(self) -> None:
+        self.assertEqual(normalize_title("  hello world  "), "Hello World")
 
-```
-def test_collapses_repeated_spaces(self) -> None:
-    self.assertEqual(normalize_title("hello   world"), "Hello World")
+    def test_collapses_repeated_spaces(self) -> None:
+        self.assertEqual(normalize_title("hello   world"), "Hello World")
 
-def test_collapses_tabs_and_newlines(self) -> None:
-    self.assertEqual(normalize_title("tree\t of\n loops"), "Tree Of Loops")
+    def test_collapses_tabs_and_newlines(self) -> None:
+        self.assertEqual(normalize_title("tree\t of\n loops"), "Tree Of Loops")
 
-def test_handles_mixed_whitespace(self) -> None:
-    self.assertEqual(
-        normalize_title("  branch   search\tengineering  "),
-        "Branch Search Engineering",
-    )
-```
+    def test_handles_mixed_whitespace(self) -> None:
+        self.assertEqual(
+            normalize_title("  branch   search\tengineering  "),
+            "Branch Search Engineering",
+        )
 
-if **name** == "**main**":
-unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
